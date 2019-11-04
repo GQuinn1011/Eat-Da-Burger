2e211cf3c39e8c0a69b638b9928271f84923d00b
Gabe Quinn <gabe.quinn@icloud.com>
2019-11-03 21:56:38 -0500

    added initial files. V1.0

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bonanza Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chocolate Habanero Burger', false);
+
+SELECT * FROM burgers;
---
+++ models/burgers.js
@@ -0,0 +1,24 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll(function(res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    insertOne: function(burger, cb) {
+        orm.insertOne(burger, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(id, cb) {
+        orm.updateOne(req, [id], function(res) {
+            cb(res);
+        });
+    },
+};
+
+// Export the database functions for the controller (burgersController.js).
+module.exports = burger;
