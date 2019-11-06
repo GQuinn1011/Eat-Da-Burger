5a6e75218226eba459d69cb7c6bde332c9315da4
Gabe Quinn <gabe.quinn@icloud.com>
2019-11-05 22:18:59 -0500

    removed list numbers from burger list

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-USE burgers_db;
+USE x13s0udh7f2aaubd;
 INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Cheese Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bonanza Burger', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Chocolate Habanero Burger', false);
